d19e2ec458b39546c92b7763e81a226866d4c1c9
Simar Kaur <89771491+simarkaur28@users.noreply.github.com>
2022-01-16 00:50:03 +0530

    Add files via upload
    content update

--- ui.R
+++ ui.R
@@ -43,21 +43,22 @@ fluidPage(theme = shinytheme("cerulean"),
                      tabPanel("Dustbins near me", leafletOutput("bbmap", height=840)),
                      tabPanel("Locality Of Dustbins", DT::dataTableOutput("data")),
                      tabPanel("Guide",sidebarPanel(
-                       tags$h1("Input:"),
+                       tags$h1("Interactive dashboard"),
+                       h3("Press 1 or 2"),
                        h2("What type is your waste?"),
-                       h3("Press 1/2"),
+                      
                        textInput("txt1", "\n1.Biodegradable \n2.Non Biodegradable", ""),
-                       textInput("txt2", "Question 2", ""),
+                       textInput("txt2", "Press 1 to know about composting. Press 2 to know about recycling", ""),
                      ), # sidebarPanel
                      mainPanel(
-                       h1("Header 1"),
+                       h2("Your Waste Management Plan!"),
                        
                        verbatimTextOutput("txtout"),
                        verbatimTextOutput("txtout2"),
-                       h3("Biodegradable Waste: "),
-                       h4("Biodegradable waste is any product that can be easily broken down naturally by water, oxygen, the sun's rays, radiation, or microorganisms. It is also referred to as green waste, recyclable waste, food waste or organic waste. Other biodegradable wastes include human waste, manure, sewage, slaughterhouse waste. This type of waste is used for composting. This process turns the waste into humus. By the process of anaerobic digestion, it can also be used as a source of energy in the form of heat, electricity and fuel.                                                                                        Few examples of Biodegradable waste : Paper waste, Human waste, Manure, Sewage waste, Hospital waste, Dead animals and plants ."),
+                       h3("Biodegradable Waste i.e Green waste, Food waste or Organic waste: "),
+                       h4("Biodegradable waste is any product that can be easily broken down naturally by water, oxygen, the sun's rays, radiation, or microorganisms.                                                                                   Examples: For composting: Human waste, manure, sewage, slaughterhouse waste.\n Paper waste, Manure, Sewage waste, Hospital waste, Dead animals and plants."),
                        h3("Non Biodegradable: "),
-                       h4("Non-Biodegradable waste includes items that cannot be broken down or decomposed by natural agents. This type of waste is a major contributors to air, water and soil pollution. Non-Biodegradable waste can be made useful if it is recycled properly.                                                                                        Examples : Glass, Plastic, Metallic Waste, Nuclear Waste, Synthetic Fibres, Ball point pen refills, Cans, Artificial Rubber, Artificial Polymers"),
+                       h4("Non-Biodegradable waste includes items that cannot be broken down or decomposed by natural agents.                                                                                       Examples : Glass, Plastic, Metallic Waste, Nuclear Waste, Synthetic Fibres, Ball point pen refills, Cans, Artificial Rubber, Artificial Polymers"),
                        
                      ) # mainPanel
                      ), # Navbar 1, tabPanel     
@@ -67,17 +68,17 @@ fluidPage(theme = shinytheme("cerulean"),
                      
                      #tabPanel("Navbar 1", file("txtout")),
                      
-          
-           tabPanel("Histogram Comparisons", tabsetPanel(
-                         tabPanel("Dustbin Hostogram", plotOutput("plotdustbins")),#basicPage(
-          #                   h1("R Shiny Bar Plot"),
-          #                   plotOutput("plot")
-          #                   
-          #                 )),
-                            tabPanel("Landmark Hostogram", plotOutput("plotlandmarks")),
-                            tabPanel("Explanation", includeMarkdown("Explanation.md"))
-                         )),
-          tabPanel("Read Me",includeMarkdown("readme.md"))
-      
+                     
+                     tabPanel("Histogram Comparisons", tabsetPanel(
+                       tabPanel("Dustbin Histogram", plotOutput("plotdustbins")),#basicPage(
+                       #                   h1("R Shiny Bar Plot"),
+                       #                   plotOutput("plot")
+                       #                   
+                       #                 )),
+                       tabPanel("Landmark Histogram", plotOutput("plotlandmarks")),
+                       tabPanel("Explanation", includeMarkdown("Explanation.md"))
+                     )),
+                     tabPanel("Read Me",includeMarkdown("readme.md"))
+                     
           )
 )
