016fcebf1e082491c3c008119dc8a7bab578244c
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-01-17 12:31:08 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(shinythemes)
 
 fluidPage(theme = shinytheme("cosmo"),
           navbarPage("Varanasi Analysis", id="main",
-                     tabPanel("Dustbins near me", leafletOutput("bbmap", height=840)),
+                     tabPanel("Dustbins Distribution", leafletOutput("bbmap", height=840)),
                      tabPanel("Locality Of Dustbins", DT::dataTableOutput("data")),
                      tabPanel("Guide",sidebarPanel(
                        tags$h1("Interactive dashboard"),
